8be08aea8bf9498afb29d3bfa0cadd1aa176f4a3
Yih Tsern <sim_yihtsern@yahoo.com>
2021-11-25 16:20:01 +0800

    v-init attribute that mimics Alpine's x-init attribute.

--- vue-alpine.js
+++ vue-alpine.js
@@ -17,6 +17,12 @@ document.addEventListener('DOMContentLoaded', () => {
                         // v-data has already been manually processed
                         // this is hack to silence Vue's warning
                     }
+                },
+                init: {
+                    inserted(el, binding, vnode) {
+                        // v-init is to enable evaluation of arbitrary expression, so there is nothing to do here
+                        // since binding.expression will be auto-evaluated by Vue
+                    }
                 }
             }
         });
