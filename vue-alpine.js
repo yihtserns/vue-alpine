fd1b417a700fd5db2358d70279c231a0a875786d
Yih Tsern <sim_yihtsern@yahoo.com>
2021-11-25 18:00:17 +0800

    v-effect attribute that mimics Alpine's x-effect attribute.

--- vue-alpine.js
+++ vue-alpine.js
@@ -23,6 +23,14 @@ document.addEventListener('DOMContentLoaded', () => {
                         // v-init is to enable evaluation of arbitrary expression, so there is nothing to do here
                         // since binding.expression will be auto-evaluated by Vue
                     }
+                },
+                effect: {
+                    bind(el, binding, vnode) {
+                        binding.value.bind(window)();
+                    },
+                    update(el, binding, vnode) {
+                        binding.value.bind(window)();
+                    }
                 }
             },
             methods: {
