2ecdff732f4b886d6790b59e65c53c3235624516
Yih Tsern <sim_yihtsern@yahoo.com>
2021-11-30 23:50:30 +0800

    Add `console` and `window` properties, to make v-effect more like Alpine's x-effect.

--- vue-alpine.js
+++ vue-alpine.js
@@ -11,26 +11,22 @@ document.addEventListener('DOMContentLoaded', () => {
             data() {
                 return data;
             },
+            computed: {
+                console: () => console,
+                window: () => window
+            },
             directives: {
                 data: {
-                    bind() {
-                        // v-data has already been manually processed
-                        // this is hack to silence Vue's warning
-                    }
+                    // v-data has already been manually processed
+                    // this is hack to silence Vue's warning
                 },
                 init: {
-                    bind(el, binding, vnode) {
-                        // v-init is to enable evaluation of arbitrary expression, so there is nothing to do here
-                        // since binding.expression will be auto-evaluated by Vue
-                    }
+                    // v-init is to enable evaluation of arbitrary expression, so there is nothing to do here
+                    // since binding.expression will be auto-evaluated by Vue
                 },
                 effect: {
-                    bind(el, binding, vnode) {
-                        binding.value.bind(window)();
-                    },
-                    update(el, binding, vnode) {
-                        binding.value.bind(window)();
-                    }
+                    // v-init is to enable evaluation of arbitrary expression, so there is nothing to do here
+                    // since binding.expression will be auto-evaluated by Vue
                 }
             },
             methods: {
